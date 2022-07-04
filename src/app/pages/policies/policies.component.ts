97522a84a25f3458a710978f6849b7418de5d097
gpazuch <1490938+gpazuch@users.noreply.github.com>
2022-07-04 19:56:15 -0300

    wip

--- src/app/pages/policies/policies.component.ts
+++ src/app/pages/policies/policies.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { combineLatest, map, Observable, startWith } from 'rxjs';
+import {
+  FilterOption,
+  FilterTypes,
+} from 'src/app/components/filter/filter.component';
+import { FilterService } from 'src/app/services/filter.service';
 import { AgentPolicy } from 'src/app/services/interfaces/policy.interface';
 import { OrbService } from 'src/app/services/orb.service';
 
@@ -9,10 +14,48 @@ import { OrbService } from 'src/app/services/orb.service';
   styleUrls: ['./policies.component.scss'],
 })
 export class PoliciesComponent implements OnInit {
+  filterOptions: FilterOption[];
+
+  filters$!: Observable<FilterOption[]>;
+
   policies$: Observable<AgentPolicy[]>;
 
-  constructor(private orb: OrbService) {
+  filteredPolicies$: Observable<AgentPolicy[]>;
+
+  addItem = true;
+
+  constructor(private orb: OrbService, private filters: FilterService) {
     this.policies$ = orb.getPolicyListView();
+    this.filters$ = filters.getFilters().pipe(startWith([]));
+
+    this.filteredPolicies$ = combineLatest({
+      policies: this.policies$,
+      filters: this.filters$,
+    }).pipe(
+      map(({ policies, filters }) => {
+        let filtered = policies;
+        filters.forEach((filter) => {
+          filtered = filtered.filter((value) => {
+            const paramValue = filter.param;
+            const result = filter.filter(value, paramValue);
+            return result;
+          });
+        });
+
+        return filtered;
+      })
+    );
+
+    this.filterOptions = [
+      {
+        name: 'Name',
+        prop: 'name',
+        filter: (policy: AgentPolicy, name: string) => {
+          return policy.name?.includes(name);
+        },
+        type: FilterTypes.Input,
+      },
+    ];
   }
 
   ngOnInit(): void {}
