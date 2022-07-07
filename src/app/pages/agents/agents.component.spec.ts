581570a7cc74af656b4a584cdafe29a3daa104f2
gpazuch <1490938+gpazuch@users.noreply.github.com>
2022-07-07 17:06:34 -0300

    wip

--- src/app/pages/agents/agents.component.spec.ts
+++ src/app/pages/agents/agents.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
 import { FilterService } from 'src/app/services/filter.service';
 import { OrbService } from 'src/app/services/orb.service';
 
@@ -12,7 +13,7 @@ describe('AgentsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AgentsComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, MatDialogModule],
       providers: [OrbService, FilterService],
     }).compileComponents();
   });
